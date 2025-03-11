41cee4a8fad4aa1e2cc3c857663e59b7c77f7454
sebastianmdahl <sebastian.moen.dahl@hotmail.com>
2025-03-11 18:36:36 +0100

    trying to fix github pages2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/excercise9/", // ✅ Set this to match your GitHub repo name
+  base: "/excercise9/", // ✅ Set this to your repository name
 });
