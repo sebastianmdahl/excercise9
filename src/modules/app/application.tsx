8735a4c941e982632b721cc650a8c3d7aa28f65f
sebastianmdahl <sebastian.moen.dahl@hotmail.com>
2025-03-11 17:32:32 +0100

    added a style to drawing layer

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -6,6 +6,11 @@ import { useGeographic } from "ol/proj";
 
 // Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
+import VectorSource from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+import { Draw } from "ol/interaction";
+
+import { Style, Fill, Stroke, Circle as CircleStyle } from "ol/style";
 
 // By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
 //  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
@@ -13,6 +18,7 @@ useGeographic();
 
 // Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
 //  map data structure will be used
+
 const map = new Map({
   // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
   //   with a certain zoom level
@@ -21,6 +27,20 @@ const map = new Map({
   layers: [new TileLayer({ source: new OSM() })],
 });
 
+const pointStyle = new Style({
+  image: new CircleStyle({
+    radius: 6,
+    fill: new Fill({ color: "red" }),
+    stroke: new Stroke({ color: "black", width: 2 }),
+  }),
+});
+
+const vectorSource = new VectorSource();
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+  style: pointStyle,
+});
+map.addLayer(vectorLayer);
 // A functional React component
 export function Application() {
   // `useRef` bridges the gap between JavaScript functions that expect DOM objects and React components
@@ -31,6 +51,23 @@ export function Application() {
     map.setTarget(mapRef.current!);
   }, []);
 
-  // This is the location (in React) where we want the map to be displayed
-  return <div ref={mapRef}></div>;
+  const enableDrawing = () => {
+    const draw = new Draw({
+      source: vectorSource,
+      type: "Point",
+    });
+    map.addInteraction(draw);
+
+    vectorSource.on("addfeature", () => {
+      map.removeInteraction(draw);
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={enableDrawing}>Draw Point</button>{" "}
+      {/* ✅ ADD THIS BUTTON */}
+      <div ref={mapRef} style={{ width: "100%", height: "500px" }}></div>
+    </div>
+  );
 }
