627ca4c0a5fec71d5b72e8e52b28a14962404b29
sebastianmdahl <sebastian.moen.dahl@hotmail.com>
2025-03-11 16:57:35 +0100

    basic react application

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Application } from "./modules/app/application";
+
+createRoot(document.getElementById("root")!).render(<Application />);
